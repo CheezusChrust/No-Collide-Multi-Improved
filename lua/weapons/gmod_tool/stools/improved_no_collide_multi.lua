94ffa750837c83adcdf6734f4bdc81d29d0d646b
CheezusChrust <rcheeze1337@gmail.com>
2018-08-28 12:41:00 -0400

    Added constraint limit

--- lua/weapons/gmod_tool/stools/improved_no_collide_multi.lua
+++ lua/weapons/gmod_tool/stools/improved_no_collide_multi.lua
@@ -7,6 +7,7 @@ TOOL.ConfigName		= nil
 TOOL.SelectedEntities = {}
 TOOL.SelectColor = Color(0, 255, 0, 255)
 TOOL.ClientConVar[ "distance" ] = "1.5"
+TOOL.ClientConVar[ "maxnocollides" ] = "512"
 TOOL.ClientConVar[ "dumb" ] = "0"
 TOOL.Smack = {
 	"Thats a lotta constraints. Why do you need dumb on?",
@@ -21,6 +22,8 @@ if (CLIENT) then
 	language.Add("tool.improved_no_collide_multi.0", "Primary: Select or unselect an entity. Hold E to get all constrained objects.  Secondary: Apply no collide to selected objects.  Reload: Clear selection.")
 	language.Add("tool.improved_no_collide_multi.distance", "Distance:")
 	language.Add("tool.improved_no_collide_multi.distance.help", "Distance scales the hitbox of objects by a certain amount when determining if they're intersecting or near eachother. Lower means less constraints, but a higher chance of props colliding. Higher means more constraints, but more more 'play' is allowed between props.")
+	language.Add("tool.improved_no_collide_multi.maxnocollides", "Max No-Collides:")
+	language.Add("tool.improved_no_collide_multi.maxnocollides.help", "The maximum number of constraints allowed to be created before the tool complains to you.")
 	language.Add("tool.improved_no_collide_multi.dumb", "Dumb No-Collide")
 	language.Add("tool.improved_no_collide_multi.dumb.help", "Turning this on disables the 'Improved' portion of this tool, thus I recommend keeping this OFF. Turn this on and enjoy (n^2)/2 constraints.")
 end
@@ -111,6 +114,14 @@ function TOOL:deselectedIt( Entity )
 	Entity:SetColor( EntityColor )
 end
 
+function TOOL:deselectAll( )
+	for Entity, _ in pairs ( self.SelectedEntities ) do
+		if self:isReallyValid( Entity) then
+			if self.SelectedEntities[ Entity ] then self:deselectedIt( Entity ) end
+		end
+	end
+end
+
 --Tool stuff
 
 function TOOL:LeftClick( trace )
@@ -152,25 +163,31 @@ function TOOL:RightClick( trace )
 	local Distance = math.Clamp( self:GetClientNumber( "distance" ), 1, 3 )
 	local Undo = self:GetClientNumber( "undo" )
 	local Dumb = self:GetClientNumber( "dumb" )
+	local MaxNoCollides = math.Clamp( self:GetClientNumber( "maxnocollides" ), 1, 2048 )
 	local NoCollidedTo = {}
 	local Constraints = {}
 
 	if table.Count( self.SelectedEntities ) == 1 then
-		notifyPlayer( Owner, "Only one prop was selected. The hell do you want me to do with it, no-collide it to itself or something?", "NOTIFY_GENERIC")
+		notifyPlayer( Owner, "Only one prop was selected. The hell do you want me to do with it, no-collide it to itself or something?", "NOTIFY_GENERIC" )
 		for Entity, _ in pairs ( self.SelectedEntities ) do
 			if self:isReallyValid( Entity) then self:deselectedIt( Entity ) end
 		end
 		return false
 	end
 
-	--if Dumb == 1 and table.Count( self.SelectedEntities )^2 > 2048 then
+	print((table.Count( self.SelectedEntities )^2)/2)
+	print(MaxNoCollides)
+	if Dumb == 1 and (table.Count( self.SelectedEntities )^2)/2 > MaxNoCollides then
+		notifyPlayer( Owner, "WARNING: Constraint count will exceed max allowed constraints (" .. math.floor((table.Count( self.SelectedEntities )^2)/2) .. ">" .. MaxNoCollides .. ")! Increase the limit, deselect dumb, or select fewer props!", "NOTIFY_GENERIC")
+		return false
+	end
 
 	local n = 0
 	for Entity, _ in pairs( self.SelectedEntities ) do
 		for Entity2, _ in pairs( self.SelectedEntities ) do
 			if (NoCollidedTo[Entity] == Entity2 or NoCollidedTo[Entity2] == Entity) and Dumb == 0 then break end
 			if (Entity == Entity2) == false then
-				if self:isReallyValid( Entity ) and self:isReallyValid( Entity2 ) and (intersection3d( Entity, Entity2, Distance ) or Dumb == 1) and n < 2048 then
+				if self:isReallyValid( Entity ) and self:isReallyValid( Entity2 ) and (intersection3d( Entity, Entity2, Distance ) or Dumb == 1) and n < MaxNoCollides then
 					NoCollidedTo[Entity] = Entity2
 					NoCollidedTo[Entity2] = Entity
 					table.insert( Constraints, constraint.NoCollide( Entity, Entity2, 0, 0 ) )
@@ -180,26 +197,33 @@ function TOOL:RightClick( trace )
 		end
 	end
 
+	if n > MaxNoCollides-1 then
+		notifyPlayer( Owner, "WARNING: Could not create amount of constraints required! Increase the constraint limit!", "NOTIFY_GENERIC" )
+		for _,Constraint in pairs( Constraints ) do
+			if Constraint and Constraint:IsValid( ) then
+				Constraint:Input( "EnableCollisions", nil, nil, nil )
+				Constraint:Remove( )
+				Value = nil
+			end
+		end
+		return false
+	end
+
 	if n > 0 then undoMultiNoCollide( Owner, Constraints ) end
 	
 	if n == 0 then
-		notifyPlayer( Owner, "No props were no-collided! It seems like all of your entities are too far apart. Try increasing the distance setting if needed.", "NOTIFY_GENERIC")
-		for Entity, _ in pairs ( self.SelectedEntities ) do
-			if self:isReallyValid( Entity) then self:deselectedIt( Entity ) end
-		end
+		notifyPlayer( Owner, "No props were no-collided! It seems like all of your entities are too far apart. Try increasing the distance setting if needed.", "NOTIFY_GENERIC" )
+		self:deselectAll()
 	else
 		if Dumb == 1 and n > 200 then
-			notifyPlayer( Owner, "No-Collided selected entities using " .. n .. " constraints. " .. self.Smack[math.random( 1, table.Count( self.Smack ) )], "NOTIFY_GENERIC" )
+			notifyPlayer( Owner, "No-Collided selected entities using " .. math.floor(n/2) .. " constraints. " .. self.Smack[math.random( 1, table.Count( self.Smack ) )], "NOTIFY_GENERIC" )
 		else
 			notifyPlayer( Owner, "No-Collided selected entities using " .. n .. " constraints", "NOTIFY_GENERIC" )
 		end
-		for Entity, _ in pairs ( self.SelectedEntities ) do
-			if self:isReallyValid( Entity) then
-				if self.SelectedEntities[ Entity ] then self:deselectedIt( Entity ) end
-			end
-		end
+		self:deselectAll()
 	end
-	self.SelectedEntities = {} --Lazy temporary fix I'll do a better one later
+
+	self.SelectedEntities = { }
 end
 
 function TOOL:Reload( trace )
@@ -207,9 +231,7 @@ function TOOL:Reload( trace )
 
 	local Owner = self:GetOwner( )
 
-	for Entity, _ in pairs( self.SelectedEntities ) do
-		if self:isSelected( Entity ) and self:isReallyValid( Entity ) then self:deselectedIt( Entity ) end
-	end
+	self:deselectAll()
 	
 	self.SelectedEntities = { }
 	
@@ -227,6 +249,13 @@ function TOOL.BuildCPanel(cp)
 	    Min = "1",
 	    Max = "3",
 	    Command = "improved_no_collide_multi_distance",
+		Help = true
+	})
+	cp:AddControl("Slider", {
+	    Label = "#tool.improved_no_collide_multi.maxnocollides",
+	    Min = "256",
+	    Max = "2048",
+	    Command = "improved_no_collide_multi_maxnocollides",
 		Help = true
 	})
 	cp:AddControl("CheckBox", {

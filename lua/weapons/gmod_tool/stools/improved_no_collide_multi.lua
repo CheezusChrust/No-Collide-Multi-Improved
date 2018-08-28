2357183af57795583753fcbd8dffe043e4308285
CheezusChrust <rcheeze1337@gmail.com>
2018-08-28 11:53:27 -0400

    Fixed singular nocollides not being undone, and fixed props at odd angles not having proper detection boxes

--- lua/weapons/gmod_tool/stools/improved_no_collide_multi.lua
+++ lua/weapons/gmod_tool/stools/improved_no_collide_multi.lua
@@ -30,32 +30,34 @@ function intersection1d(box1,box2)
 end
 
 function intersection3d(e1,e2,sizeMul)
+	local e1_AABBMins,e1_AABBMaxs = e1:GetRotatedAABB(e1:OBBMins(),e1:OBBMaxs())
+	local e2_AABBMins,e2_AABBMaxs = e2:GetRotatedAABB(e2:OBBMins(),e2:OBBMaxs())
     local box1 = {
         x = {
-            min = e1:LocalToWorld(e1:OBBMins()*sizeMul).x,
-            max = e1:LocalToWorld(e1:OBBMaxs()*sizeMul).x
+            min = (e1:GetPos() + e1_AABBMins*sizeMul).x,
+            max = (e1:GetPos() + e1_AABBMaxs*sizeMul).x
         },
         y = {
-            min = e1:LocalToWorld(e1:OBBMins()*sizeMul).y,
-            max = e1:LocalToWorld(e1:OBBMaxs()*sizeMul).y
+            min = (e1:GetPos() + e1_AABBMins*sizeMul).y,
+            max = (e1:GetPos() + e1_AABBMaxs*sizeMul).y
         },
         z = {
-            min = e1:LocalToWorld(e1:OBBMins()*sizeMul).z,
-            max = e1:LocalToWorld(e1:OBBMaxs()*sizeMul).z
+            min = (e1:GetPos() + e1_AABBMins*sizeMul).z,
+            max = (e1:GetPos() + e1_AABBMaxs*sizeMul).z
         }
     }
     local box2 = {
         x = {
-            min = e2:LocalToWorld(e2:OBBMins()*sizeMul).x,
-            max = e2:LocalToWorld(e2:OBBMaxs()*sizeMul).x
+            min = (e2:GetPos() + e2_AABBMins*sizeMul).x,
+            max = (e2:GetPos() + e2_AABBMaxs*sizeMul).x
         },
         y = {
-            min = e2:LocalToWorld(e2:OBBMins()*sizeMul).y,
-            max = e2:LocalToWorld(e2:OBBMaxs()*sizeMul).y
+            min = (e2:GetPos() + e2_AABBMins*sizeMul).y,
+            max = (e2:GetPos() + e2_AABBMaxs*sizeMul).y
         },
         z = {
-            min = e2:LocalToWorld(e2:OBBMins()*sizeMul).z,
-            max = e2:LocalToWorld(e2:OBBMaxs()*sizeMul).z
+            min = (e2:GetPos() + e2_AABBMins*sizeMul).z,
+            max = (e2:GetPos() + e2_AABBMaxs*sizeMul).z
         }
     }
     return intersection1d(box1.x,box2.x) and intersection1d(box1.y,box2.y) and intersection1d(box1.z,box2.z)
@@ -147,7 +149,7 @@ function TOOL:RightClick( trace )
 	if table.Count( self.SelectedEntities ) == 0 then return false end
 
 	local Owner = self:GetOwner( )
-	local Distance = math.Clamp( self:GetClientNumber( "distance" ), 1.25, 3 )
+	local Distance = math.Clamp( self:GetClientNumber( "distance" ), 1, 3 )
 	local Undo = self:GetClientNumber( "undo" )
 	local Dumb = self:GetClientNumber( "dumb" )
 	local NoCollidedTo = {}
@@ -161,14 +163,14 @@ function TOOL:RightClick( trace )
 		return false
 	end
 
-	if Dumb == 1 and table.Count( self.SelectedEntities )^2 > 2048 then
+	--if Dumb == 1 and table.Count( self.SelectedEntities )^2 > 2048 then
 
 	local n = 0
 	for Entity, _ in pairs( self.SelectedEntities ) do
 		for Entity2, _ in pairs( self.SelectedEntities ) do
 			if (NoCollidedTo[Entity] == Entity2 or NoCollidedTo[Entity2] == Entity) then break end
 			if (Entity == Entity2) == false then
-				if self:isReallyValid( Entity ) and self:isReallyValid( Entity2 ) and (intersection3d( Entity, Entity2, Distance ) or Dumb == 1) n < 2048 then
+				if self:isReallyValid( Entity ) and self:isReallyValid( Entity2 ) and (intersection3d( Entity, Entity2, Distance ) or Dumb == 1) and n < 2048 then
 					NoCollidedTo[Entity] = Entity2
 					NoCollidedTo[Entity2] = Entity
 					table.insert( Constraints, constraint.NoCollide( Entity, Entity2, 0, 0 ) )
@@ -178,7 +180,7 @@ function TOOL:RightClick( trace )
 		end
 	end
 
-	if n > 1 then undoMultiNoCollide( Owner, Constraints ) end
+	if n > 0 then undoMultiNoCollide( Owner, Constraints ) end
 	
 	if n == 0 then
 		notifyPlayer( Owner, "No props were no-collided! It seems like all of your entities are too far apart. Try increasing the distance setting if needed.", "NOTIFY_GENERIC")
@@ -222,7 +224,7 @@ function TOOL.BuildCPanel(cp)
 	cp:AddControl("Slider", {
 	    Label = "#tool.improved_no_collide_multi.distance",
 		Type = "Float",
-	    Min = "1.25",
+	    Min = "1",
 	    Max = "3",
 	    Command = "improved_no_collide_multi_distance",
 		Help = true
